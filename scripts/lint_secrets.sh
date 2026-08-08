c2564ad92cf098b223ae45a1902d04d08768d800
Brian Martin <brian@brianmartin.com>
2026-08-07 22:16:57 -0400

    results: freeze W7 candidate 7 runtime integration

--- scripts/lint_secrets.sh
+++ scripts/lint_secrets.sh
@@ -199,6 +199,8 @@ allowlist = {
     "freeze_json_sha256",
     "freeze_sha256",
     "harness_sha256",
+    "cgroup_sha256",
+    "safe_sha256",
     "harness_tests_sha256",
     "fio_result_sha256",
     "model_sha256",
